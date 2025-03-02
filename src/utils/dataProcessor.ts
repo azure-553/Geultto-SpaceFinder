c9a1df2ed2d5cd53ffa20230b3022a3daedc8ed2
azure-553 <simmijin816@gmail.com>
2025-03-02 20:11:11 +0900

    feat: Notion DB data 처리 함수 추가

---
+++ src/utils/dataProcessor.ts
@@ -0,0 +1,52 @@
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+export function extractPageData(page: PageObjectResponse) {
+  let location = "notion_data";
+  let spaceData: { [key: string]: string } = {};
+
+  Object.entries(page.properties).forEach(([key, value]) => {
+    let output = "";
+    switch (value.type) {
+      case "title":
+        output = value.title.map((item) => item.plain_text).join(" ");
+        break;
+      case "rich_text":
+        output = value.rich_text.map((item) => item.plain_text).join(" ");
+        break;
+      case "date":
+        output = value.date?.start || "";
+        break;
+      case "number":
+        output = (value.number ?? "").toString();
+        break;
+      case "select":
+        output = value.select?.name || "";
+        break;
+      case "multi_select":
+        output = value.multi_select.map((item) => item.name).join(", ");
+        break;
+      case "checkbox":
+        output = value.checkbox ? "True" : "False";
+        break;
+      case "url":
+        output = value.url ?? "";
+        break;
+      case "email":
+        output = value.email ?? "";
+        break;
+      case "phone_number":
+        output = value.phone_number ?? "";
+        break;
+      default:
+        output = `[${value.type} 데이터]`;
+    }
+
+    if (key === "Location" && output) {
+      location = output.replace(/[^a-zA-Z0-9가-힣 ]/g, "");
+    }
+
+    spaceData[key] = output;
+  });
+
+  return { location, spaceData };
+}
